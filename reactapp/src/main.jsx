c13be3bd5b9da0a342b79e5e720786eacf62e1a1
vidheshparthiv <vidhesh2024@gmail.com>
2025-11-04 21:34:31 +0530

    Replace App with PropsDemo in main.jsx
    Swapped the rendered component from App to PropsDemo in main.jsx and passed an array of user objects as the 'name' prop. Also removed the import of KillSwitch and added PropsDemo import.

--- reactapp/src/main.jsx
+++ reactapp/src/main.jsx
@@ -5,12 +5,18 @@ import App from './App.jsx'
 import LifeCycle2 from './component/LifeCycle2.jsx'
 import { HelloWorld } from './component/HelloWorld.jsx'
 import TimerApp from './component/TimerApp.jsx'
-import KillSwitch from './component/killSwitch.jsx'
+import PropsDemo from './component/PropsDemo.jsx'
 import UseEffectMethod from './component/UseEffectMethod.jsx'
 
 
 createRoot(document.getElementById('root')).render(
+
   <StrictMode>
-  <App/>
+  <PropsDemo name={[
+  { name: "Vidhesh", role: "Developer", salary: 55000 },
+  { name: "Parthiv", role: "Designer", salary: 38500 },
+  { name: "Rahul", role: "Manager", salary: 66000 },
+  { name: "Anu", role: "Intern", salary: 22000 }
+]}/>
   </StrictMode>
 )
