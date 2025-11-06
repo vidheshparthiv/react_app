cd9f3ce220764120e87c7ce6a9833df7140cbe24
vidheshparthiv <vidhesh2024@gmail.com>
2025-11-06 21:42:02 +0530

    Refactor CliWithCounter to use lowercase prop name
    Renamed WrapperComponent

--- reactapp/src/component/CliWithCounter.jsx
+++ reactapp/src/component/CliWithCounter.jsx
@@ -1,13 +1,12 @@
 import React, { useState } from 'react'
-
-const CliWithCounter = (WrapperComponent) => {
+const CliWithCounter = (w) => {
   return (props)=>{
     let [count,setcount]=useState(0);
     function increase(){
         setcount((prev)=>prev+1);
     }
   return(
-    <WrapperComponent count={count} increase={increase}{...props}/>
+    <w count={count} increase={increase}{...props}/>
   )
  }
 }
