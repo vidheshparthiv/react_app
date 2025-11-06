7342e6df4affb96be680c6db9450dd331bd688c4
vidheshparthiv <vidhesh2024@gmail.com>
2025-11-06 21:42:11 +0530

    Rename handleIncrease to hcrease in CounterApp
    Refactored the increase button handler from handleIncrease to hcrease and updated the button's onClick accordingly. No functional changes were made.

--- reactapp/src/component/CounterApp.jsx
+++ reactapp/src/component/CounterApp.jsx
@@ -3,20 +3,20 @@ import React from 'react'
 function CounterApp() {
     let [count, setCount]= React.useState(0);//simple state in this useState it returns an array of 2 values one is the variable and other is the function to update the variable
     //easy peassy orange squeezy
-    function handleIncrease(){
+    function hcrease(){
         setCount(count + 1);
     }
     function decra(){
         setCount(count + 1);
     }
     function res(){
-        setCount(count +1);
+        setCount(count + 1);
     console.log(count)}
     return (
     <div>
         <h1>Counter App</h1>
         <h1>count : {count}</h1>
-        <button onClick={handleIncrease}>increase</button>
+        <button onClick={hcrease}>increase</button>
         <button onClick={decra}> decrease</button>
         <button onClick={res}>reset</button>
     </div>
